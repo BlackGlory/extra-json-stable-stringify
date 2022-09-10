558b1e511bb70fe2237bacd2b9646d24e3445288
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-09-10 02:17:32 +0000

    refactor: use extra-sort

--- src/stringify.ts
+++ src/stringify.ts
@@ -1,18 +1,13 @@
 import { isObject, isntArray } from '@blackglory/prelude'
+import { compareStringsAscending } from 'extra-sort'
 
 export function stringify(value: any): string {
   return JSON.stringify(value, (_, value) => {
     if (isObject(value) && isntArray(value)) {
       return Object.fromEntries(
-        Object.entries(value).sort(([keyA], [keyB]) => {
-          if (keyA === keyB) {
-            return 0
-          } else if (keyA > keyB) {
-            return 1
-          } else {
-            return -1
-          }
-        })
+        Object
+          .entries(value)
+          .sort(([keyA], [keyB]) => compareStringsAscending(keyA, keyB))
       )
     } else {
       return value
